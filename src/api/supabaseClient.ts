118d2e9d66bbe8f512a7186b17b230606cac0de7
padlock4134 <162849084+padlock4134@users.noreply.github.com>
2025-06-16 22:14:58 -0400

    Update supabaseClient.ts
    rfff

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -9,4 +9,10 @@ const env = import.meta.env as ImportMetaEnv;
 const supabaseUrl = env.VITE_SUPABASE_URL;
 const supabaseAnonKey = env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    storageKey: 'porkchop-auth-token',
+  },
+});
